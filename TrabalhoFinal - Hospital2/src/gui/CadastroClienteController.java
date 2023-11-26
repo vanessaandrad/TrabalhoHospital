2748e4cdf19118bf6f5a7ac6e9f03e1490405f7e
Vanessa Andrade <vanessaandrade.sousa@aluno.uece.br>
2023-11-26 12:20:38 -0300

    cadastrar medico e paciente no mysql

--- TrabalhoFinal - Hospital2/src/gui/CadastroClienteController.java
+++ TrabalhoFinal - Hospital2/src/gui/CadastroClienteController.java
@@ -1,10 +1,14 @@
 package gui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import model.entities.Cliente;
 
 public class CadastroClienteController {
 	
@@ -29,23 +33,47 @@ public class CadastroClienteController {
 	@FXML
 	private Label labelMensagem;
 	
-	@FXML
-	public void realizarCadastroP() {
-		String nome = textFieldNome.getText();
-		String idade = textFieldIdade.getText();
-		String plano = textFieldPlano.getText();
-		String senha = textFieldSenha.getText();
-		//String CPF = textFieldCPF.getText();
-		//int cpf = Integer.parseInt(CPF);
-		
-		Cliente.cadastrarCliente(nome, idade, plano, senha);
-		
-		labelMensagem.setText("Cadastro realizado com sucesso!");
-		
-		textFieldNome.clear();
-        textFieldIdade.clear();
-        textFieldPlano.clear();
-        textFieldSenha.clear();
-        textFieldCPF.clear();
+	 @FXML
+	    public void realizarCadastroP() {
+	        String nome = textFieldNome.getText();
+	        String idade = textFieldIdade.getText();
+	        String plano = textFieldPlano.getText();
+	        String senha = textFieldSenha.getText();
+
+	        String url = "jdbc:mysql://localhost:3306/hospital";
+	        String username = "root";
+	        String password = "demilovato1";
+
+	        String insertQuery = "INSERT INTO pacientes (nome, idade, plano, senha) VALUES (?, ?, ?, ?)";
+
+	        try (Connection connection = DriverManager.getConnection(url, username, password);
+	             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+
+	            preparedStatement.setString(1, nome);
+	            preparedStatement.setString(2, idade);
+	            preparedStatement.setString(3, plano);
+	            preparedStatement.setString(4, senha);
+
+	            int rowsAffected = preparedStatement.executeUpdate();
+
+	            if (rowsAffected > 0) {
+	                labelMensagem.setText("Cadastro realizado com sucesso!");
+	                clearFields();
+	            } else {
+	                labelMensagem.setText("Falha ao cadastrar.");
+	            }
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            labelMensagem.setText("Erro ao realizar o cadastro.");
+	        }
+	    }
+
+	    private void clearFields() {
+	        textFieldNome.clear();
+	        textFieldIdade.clear();
+	        textFieldPlano.clear();
+	        textFieldSenha.clear();
+	        textFieldCPF.clear();
+	    }
 	}
-}
--- TrabalhoFinal - Hospital2/src/gui/CadastroMedicoController.java
+++ TrabalhoFinal - Hospital2/src/gui/CadastroMedicoController.java
@@ -1,10 +1,14 @@
 package gui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import model.entities.Medico;
 
 public class CadastroMedicoController {
 	
@@ -30,22 +34,51 @@ public class CadastroMedicoController {
 	private Label labelMensagem;
 	
 	@FXML
-	public void realizarCadastro() {
-		String nome = textFieldNome.getText();
-		String especialidade = textFieldEspecialidade.getText();
-		String planoAtendido = textFieldPlanoAtendido.getText();
-		String senha = textFieldSenha.getText();
-		String CRM = textFieldCRM.getText();
-		int crm = Integer.parseInt(CRM);
-		
-		Medico.cadastrarMedico(nome, especialidade, planoAtendido, senha, crm);
-		
-		labelMensagem.setText("Cadastro realizado com sucesso!");
-		
-		textFieldNome.clear();
+    public void realizarCadastro() {
+        String nome = textFieldNome.getText();
+        String especialidade = textFieldEspecialidade.getText();
+        String planoAtendido = textFieldPlanoAtendido.getText();
+        String senha = textFieldSenha.getText();
+        String CRM = textFieldCRM.getText();
+        int crm = Integer.parseInt(CRM);
+
+       
+        String url = "jdbc:mysql://localhost:3306/hospital";
+        String username = "root";
+        String password = "demilovato1";
+
+        String insertQuery = "INSERT INTO medicoscadastrados (nome, especialidade, plano_atendido, senha, crm)"
+                + " VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+
+            preparedStatement.setString(1, nome);
+            preparedStatement.setString(2, especialidade);
+            preparedStatement.setString(3, planoAtendido);
+            preparedStatement.setString(4, senha);
+            preparedStatement.setInt(5, crm);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                labelMensagem.setText("Cadastro realizado com sucesso!");
+                clearFields();
+            } else {
+                labelMensagem.setText("Falha ao cadastrar.");
+            }
+
+        } catch (SQLException | NumberFormatException e) {
+            e.printStackTrace();
+            labelMensagem.setText("Erro ao realizar o cadastro.");
+        }
+    }
+
+    private void clearFields() {
+        textFieldNome.clear();
         textFieldEspecialidade.clear();
         textFieldPlanoAtendido.clear();
         textFieldSenha.clear();
         textFieldCRM.clear();
-	}
+    }
 }
